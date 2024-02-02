5364427390dc607488a10a02d13b075b15238a2b
William Chong <me@williamchong.cloud>
2024-02-02 17:00:29 +0800

    📈 Add ig click events

--- pages/share.vue
+++ pages/share.vue
@@ -6,14 +6,18 @@
       <p class="mb-8 text-xl">和我們分享你的散步學體驗。看看其他人在散步時看到什麼。</p>
 
       <p class="text-xl font-mono">
-        <a href="https://www.instagram.com/liker.land/" target="_blank" rel="noopener">@liker.land</a>
-        <a href="https://www.instagram.com/until_coffee/" target="_blank" rel="noopener">@until_coffee</a>
+        <a @click="onClickIGPage('LikerLand')" href="https://www.instagram.com/liker.land/" target="_blank"
+          rel="noopener">@liker.land</a>
+        <a @click="onClickIGPage('UntilCoffee')" href="https://www.instagram.com/until_coffee/" target="_blank"
+          rel="noopener">@until_coffee</a>
       </p>
       <p class="text-xl font-mono">
-        <a href="https://www.instagram.com/explore/tags/咖啡散步學/" target="_blank" rel="noopener">#咖啡散步學</a>
-        <a href="https://www.instagram.com/explore/tags/urbanstrollology/" target="_blank"
-          rel="noopener">#urbanstrollology</a>
-        <a href="https://www.instagram.com/explore/tags/likerland/" target="_blank" rel="noopener">#likerland</a>
+        <a @click="onClickIgTag('Coffeecitywalk')" href="https://www.instagram.com/explore/tags/咖啡散步學/" target="_blank"
+          rel="noopener">#咖啡散步學</a>
+        <a @click="onClickIgTag('Urbanstrollology')" href="https://www.instagram.com/explore/tags/urbanstrollology/"
+          target="_blank" rel="noopener">#urbanstrollology</a>
+        <a @click="onClickIgTag('Likerland')" href="https://www.instagram.com/explore/tags/likerland/" target="_blank"
+          rel="noopener">#likerland</a>
       </p>
 
     </main>
@@ -25,6 +29,14 @@
   </div>
 </template>
 <script setup lang="ts">
+function onClickIGPage(page: string) {
+  useTrackEvent('ClickIGPage')
+}
+
+function onClickIGTag(tag: string) {
+  useTrackEvent('ClickIGTag')
+}
+
 function onClickRestart() {
   useTrackEvent('RestartFromShare')
 }
