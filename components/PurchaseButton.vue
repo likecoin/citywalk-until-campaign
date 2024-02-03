4b6d75715060b8a662ce6738d7e95f5ee733aff8
Ng Wing Tat, David <i@ngwingt.at>
2024-02-04 04:16:32 +0800

    💬 Update purchase button text

--- components/PurchaseButton.vue
+++ components/PurchaseButton.vue
@@ -5,7 +5,7 @@
     @click="onClickPurchase"
     target="_blank"
     to="https://liker.land/zh-Hant/nft/class/likenft1rvwlaaedlfewy0h4cyrfduc5xcgxh740wpavvzj885hmmjxpwzwscavef3?channel=liker_land&utm_source=purchase_button&utm_medium=web&utm_campaign=citywalk_until"
-  >購買《散步學》</UButton>
+  >購買《城市散步學》</UButton>
 </template>
 
 <script setup lang="ts">
