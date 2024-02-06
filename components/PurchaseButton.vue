9ce188cf2296a97a4b87d525fa689a7cc34ee419
Ng Wing Tat, David <i@ngwingt.at>
2024-02-06 10:22:25 +0800

    🚸 Use different label & icon for purchase button

--- components/PurchaseButton.vue
+++ components/PurchaseButton.vue
@@ -1,12 +1,14 @@
 <template>
   <UButton
+    :label="buttonLabel"
+    :icon="props.isToSummary ? '' : 'i-heroicons-arrow-top-right-on-square'"
     size="xl"
     variant="soft"
     :target="props.isToSummary ? '_self' : '_blank'"
     :to="toRoute"
     v-bind="$attrs"
     @click="onClickPurchase"
-  >購買《城市散步學》</UButton>
+  />
 </template>
 
 <script setup lang="ts">
@@ -26,6 +28,10 @@ const toRoute = computed(() => {
     return { name: 'summary' }
   }
   return 'https://liker.land/zh-Hant/nft/class/likenft1rvwlaaedlfewy0h4cyrfduc5xcgxh740wpavvzj885hmmjxpwzwscavef3?channel=liker_land&utm_source=purchase_button&utm_medium=web&utm_campaign=citywalk_until_coffee'
+})
+
+const buttonLabel = computed(() => {
+  return props.isToSummary ? '購買《城市散步學》' : '前往購買《城市散步學》'
 })
 
 function onClickPurchase() {
